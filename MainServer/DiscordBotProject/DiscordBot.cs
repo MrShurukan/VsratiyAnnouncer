b9b23ab744d6cc4d7e3c080eadd05d7be2b54fb8
Ilya Zavyalov <ilya.zavyalov0@gmail.com>
2023-11-09 01:42:45 +0300

    Это победа - играются mp3 файлы из папки MainServer/Overlord/Sounds/ с админ панели

--- MainServer/DiscordBotProject/DiscordBot.cs
+++ MainServer/DiscordBotProject/DiscordBot.cs
@@ -1,26 +1,50 @@
-﻿using Discord;
+﻿using System.Diagnostics;
+using System.Reflection;
+using Discord;
+using Discord.Audio;
 using Discord.Commands;
+using Discord.Interactions;
 using Discord.Net;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 namespace DiscordBotProject;
 
 public class DiscordBot
 {
-    public static ulong? TestChannelId { get; private set; } = null;
-    private DiscordSocketClient _client = new DiscordSocketClient();
+    private readonly DiscordSocketClient _client = new();
+    private InteractionService _interactionService = default!;
+
+    private readonly IServiceProvider _serviceProvider = new ServiceCollection().BuildServiceProvider();
     private Task Log(LogMessage msg)
     {
         Console.WriteLine(msg.ToString());
         return Task.CompletedTask;
     }
-    
+
     public async Task StartAsync()
     {
         _client.Log += Log;
         // _client.Ready += ClientReady;
-        _client.SlashCommandExecuted += SlashCommandHandler;
+        // _client.SlashCommandExecuted += SlashCommandHandler;
+
+        _interactionService = new InteractionService(_client.Rest);
+        try
+        {
+            await _interactionService.AddModulesAsync(typeof(MainInteractionModule).Assembly, _serviceProvider);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n\nERROR WHILE ADDING MODULES: " + e.Message);
+            throw;
+        }
+
+        _client.InteractionCreated += async (x) =>
+        {
+            var ctx = new SocketInteractionContext(_client, x);
+            await _interactionService.ExecuteCommandAsync(ctx, _serviceProvider);
+        };
 
         const string settingsPath = $"../{nameof(DiscordBotProject)}/discordbot.json";
         if (!File.Exists(settingsPath))
@@ -28,7 +52,7 @@ public class DiscordBot
             throw new FileNotFoundException($"Нет файла discordbot.json в корне проекта {nameof(DiscordBotProject)} (см. discordbot.sample.json)");
         }
         var discordToken = JsonConvert.DeserializeObject<Settings>(await File.ReadAllTextAsync(settingsPath)).Token;
-        
+
         Console.WriteLine("Запускаю бота...");
         await _client.LoginAsync(TokenType.Bot, discordToken);
         await _client.StartAsync();
@@ -36,124 +60,22 @@ public class DiscordBot
         // Block this task until the program is closed.
         await Task.Delay(-1);
     }
-    private async Task SlashCommandHandler(SocketSlashCommand command)
-    {
-        switch (command.Data.Name)
-        {
-            case "ping":
-                await PingCommandHandler(command);
-                break;
-            case "test-remember-channel":
-                await TestRememberChannelHandler(command);
-                break;
-            case "start":
-                await StartCommandHandler(command);
-                break;
-            case "stop":
-                await StopCommandHandler(command);
-                break;
-        }
 
+    public async Task CreateSlashCommandsAsync(ulong guildId)
+    {
+        await _interactionService.RegisterCommandsToGuildAsync(guildId);
     }
-    private async Task TestRememberChannelHandler(SocketSlashCommand command)
+    public async Task SendTestMessageToMemorizedChannelAsync(string customMessage)
     {
-        TestChannelId = command.Channel.Id;
-        await command.RespondAsync(text: $"Успешно запомнил канал {TestChannelId}", ephemeral: true);
-    }
-    private async Task StartCommandHandler(SocketSlashCommand command)
-    {
-        var user = command.User as SocketGuildUser;
-        if (user!.VoiceState == null)
-        {
-            await command.RespondAsync("Дядя, в канал-то зайди голосовой ежжи", ephemeral: true);
-            return;
-        }
-
-        await command.RespondAsync("Стартуем, пацаны!");
-        Task.T
-        await user.VoiceState.Value.VoiceChannel.ConnectAsync();
-    }
-    private async Task StopCommandHandler(SocketSlashCommand command)
-    {
-        var guild = _client.GetGuild((ulong)command.GuildId!);
-        var audioClient = guild.AudioClient;
-    
-        if (audioClient != null)
-        {
-            // Stop the audio client if needed (e.g., if it's playing or streaming something)
-            // ...
-
-            // Disconnect the audio client
-            await audioClient.StopAsync();
-            await command.RespondAsync("А на сегодня всё...");
-        }
-        else
-        {
-            await command.RespondAsync("Дядя, у тебя шиза, я даже к каналу не подключен никакому", ephemeral: true);
-        }
-    }
-
-    private async Task PingCommandHandler(SocketSlashCommand command)
-    {
-        var user = command.User as SocketGuildUser;
-        await command.RespondAsync($"Запущена команда '{command.Data.Name}'. "
-                                   + $"Получено от {command.User.Username}. "
-                                   + $"В канале {command.Channel.Name}. "
-                                   + $"Пользователь {(user?.VoiceState != null ? $"В голосовом канале ({user.VoiceState})" : "не в голосовом канале")}.");
-    }
-
-    public async Task CreateSlashCommands(ulong guildId)
-    {
-        var guild = _client.GetGuild(guildId);
-
-        var commands = new List<SlashCommandBuilder>
-        {
-            // ping
-            new SlashCommandBuilder()
-                .WithName("ping")
-                .WithDescription("Тестовая команда для проверки бота"),
-            // test-remember-channel
-            new SlashCommandBuilder()
-                .WithName("test-remember-channel")
-                .WithDescription("[Админ] Данная команда нужна для проверки связки между Overlord и DiscordBot"),
-            new SlashCommandBuilder()
-                .WithName("start")
-                .WithDescription("Да начнётся треш в голосовом канале"),
-            new SlashCommandBuilder()
-                .WithName("stop")
-                .WithDescription("Хватит на сегодня интернета")
-        };
-
-        try
-        {
-            foreach (var command in commands)
-            {
-                await guild.CreateApplicationCommandAsync(command.Build());
-            }
-        }
-        catch(HttpException exception)
-        {
-            // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
-            var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
-
-            // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
-            Console.WriteLine("Exception occured when creating guild commands");
-            Console.WriteLine(json);
-
-            throw;
-        }
-    }
-    public async Task SendTestMessageToMemorizedChannel(string customMessage)
-    {
-        if (TestChannelId is null) 
+        if (MainInteractionModule.TestChannelId is null)
             throw new Exception("Нет канала для отправки (нужна команда 'test-remember-channel')");
 
-        var channel = await _client.GetChannelAsync((ulong)TestChannelId);
+        var channel = await _client.GetChannelAsync((ulong)MainInteractionModule.TestChannelId);
         if (channel is null)
-            throw new Exception($"Не удалось найти канал с id {TestChannelId}");
+            throw new Exception($"Не удалось найти канал с id {MainInteractionModule.TestChannelId}");
 
         if (channel is not ITextChannel textChannel)
-            throw new Exception($"Это не текстовый канал ({TestChannelId})!");
+            throw new Exception($"Это не текстовый канал ({MainInteractionModule.TestChannelId})!");
 
         await textChannel.SendMessageAsync($"ХА! Лови сообщение от админа: {customMessage}");
     }
